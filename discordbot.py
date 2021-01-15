9537c01c06f3ff3ee63ac9dd4c5d04a17cf7eff2
hajiri0319 <77188651+hajiri0319@users.noreply.github.com>
2021-01-15 15:46:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,67 @@
-from discord.ext import commands
+import random
+import discord
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if client.user != message.author:
+        dice = random.randint(1,5)
+        if dice == 1:
+            if '(´◉◞౪◟◉)' in message.content:
+                m = "こっち見んな()"
+                await message.channel.send(m)
+            if 'ムワア' in message.content:
+                m = "ムワアアアアアアア！！！！"
+                await message.channel.send(m)
+            if  'ンアー' in message.content:
+                m = "ンアーッ!(≧Д≦)"
+                await message.channel.send(m)
+            if 'ムシャムシャ' in message.content:
+                m = "ムワアアアアアアア！！！！"
+                await message.channel.send(m)
+            if 'ﾑｼｬﾑｼｬ' in message.content:
+                m = "ムワアアアアアアア！！！！"
+                await message.channel.send(m)
+            if 'シモビト' in message.content:
+                m = "ムワアアアアアアア！！！！"
+                await message.channel.send(m)
+        if 'dice' in message.content:
+                    await message.channel.send(dice)
+        if message.content == "おみくじ":
+            num = random.randint(0,60)
+            if num < 2:
+                m = "おみくじ結果→大吉"
+                await message.channel.send(m)
+            elif 2 <= num < 10:
+                m = "おみくじ結果→中吉"
+                await message.channel.send(m)
+            elif 10 <= num < 20:
+                m = "おみくじ結果→小吉"
+                await message.channel.send(m)
+            elif 20 <= num < 40:
+                m = "おみくじ結果→吉"
+                await message.channel.send(m)
+            elif 40 <= num < 50:
+                m = "おみくじ結果→末吉"
+                await message.channel.send(m)
+            elif 50 <= num < 55:
+                m = "おみくじ結果→凶"
+                await message.channel.send(m)
+            elif 55 <= num < 58:
+                m = "おみくじ結果→中凶"
+                await message.channel.send(m)
+            else:
+                m = "おみくじ結果→大凶"
+                await message.channel.send(m)      
+
+
 token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
